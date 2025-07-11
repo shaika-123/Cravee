0c272cad782c818fb96211ecbd2b6d8f9d4050b3
Afifa Shaik <afifashaik248@gmail.com>
2025-07-11 16:53:51 +0530

    Revert to direct working backend URL

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = import.meta.env.VITE_BACKEND_URL;
+  const url = "http://localhost:4000";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
