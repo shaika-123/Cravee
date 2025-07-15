e218425798f38d920c2a2b1a487376255d9aca79
Afifa Shaik <afifashaik248@gmail.com>
2025-07-15 14:44:20 +0530

    changes

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,4 +1,5 @@
 import express from "express";
+import multer from "multer";
 import { addFood, listFood, removeFood } from "../controllers/foodController.js";
 import upload from "../middleware/multer.js";
 
@@ -13,6 +14,26 @@ const handleMulterError = (error, req, res, next) => {
         message: 'File too large. Maximum size is 5MB.'
       });
     }
+    
+    if (error.code === 'LIMIT_FILE_COUNT') {
+      return res.status(400).json({
+        success: false,
+        message: 'Too many files uploaded.'
+      });
+    }
+    
+    if (error.code === 'LIMIT_UNEXPECTED_FILE') {
+      return res.status(400).json({
+        success: false,
+        message: 'Unexpected file field.'
+      });
+    }
+    
+    // Handle other multer errors
+    return res.status(400).json({
+      success: false,
+      message: 'File upload error: ' + error.message
+    });
   }
   
   if (error.message === 'Only image files are allowed!') {
